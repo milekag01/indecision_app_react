c210df14d227e8c5f1822869634ab7c1a43ac2fa
milekag01 <milekag01@gmail.com>
2018-09-18 18:32:09 +0530

    method binding

--- src/app.js
+++ src/app.js
@@ -40,8 +40,12 @@ class Action extends React.Component {
 } 
 
 class Options extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
     handleRemoveAll() {
-        alert("remove all");
+        console.log(this.props.options);
     }
     render() {
         return (
