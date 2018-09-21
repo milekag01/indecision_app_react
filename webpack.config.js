22bc1a3799db274ada47b2f0ef95b67ec3ddfacb
milekag01 <milekag01@gmail.com>
2018-09-22 05:08:11 +0530

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// we need path of input file and output file
+const path = require('path');
+
+// console.log(path.join(__dirname, 'public'));
+
+module.exports = {
+    entry: './src/app.js',
+    output:{
+        path: path.join(__dirname, 'public'),
+        filename:'bundle.js'
+    }
+}
