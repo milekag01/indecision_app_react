1d0cbddf541e0fe4b542debd764783c5f32081d2
milekag01 <milekag01@gmail.com>
2018-09-23 07:05:24 +0530

    webpack-dev-server

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -10,7 +10,7 @@ export default class AddOption extends React.Component {
     }
     handleAddOption(event) {
         event.preventDefault();
-
+        
         const option = event.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);    
     
--- webpack.config.js
+++ webpack.config.js
@@ -15,5 +15,9 @@ module.exports = {
             test: /\.js$/,
             exclude : /node_modules/
         }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
     }
 }
